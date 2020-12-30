0b55d17ecec54432886da34e5295f40f37971af4
riccdigital <eryc.mabonga@gmail.com>
2020-12-30 11:30:31 +0300

    JS setup

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,11 @@
 //event listener to make script wait for html to finish loading
 document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const width = 10
+    const scoreDisplay = document.querySelector('#score')
+    const startBtn = document.querySelector('#start-btn')
+
     
 
 
