0e047e30acd96cae5203d8981caf13f5dab88fe5
riccdigital <eryc.mabonga@gmail.com>
2020-12-30 15:28:28 +0300

    shape rotation function

--- scripts/main.js
+++ scripts/main.js
@@ -75,7 +75,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if (e.keyCode === 37) {
             moveLeft();
         } else if (e.keyCode === 38) {
-            //rotate()
+            rotate();
         }
          else if (e.keyCode === 39 ) {
             moveRight();
@@ -129,9 +129,19 @@ document.addEventListener('DOMContentLoaded', () => {
         if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
             currentPosition -= 1;
         }
-        draw();
+        draw();    
 
-        
+    }
+
+    //rotate the shape
+    function rotate() {
+        undraw();
+        currentRotation++;
+        if (currentRotation === current.length) {
+            currentRotation = 0;
+        }
+        current = theShapes[random][currentRotation]
+        draw();
 
     }
 
